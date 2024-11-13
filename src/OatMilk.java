795b335f1004c9662cc64e0d624fe4994402d848
Daniel Norling <dnorling85@gmail.com>
2024-11-13 14:21:30 +0100

    Added class constructors and getters

--- src/OatMilk.java
+++ src/OatMilk.java
@@ -1,4 +1,40 @@
 public class OatMilk
 {
+    private double oatFat;
+    private double oatLiter;
+
+    public OatMilk(double oatFat, double oatLiter)
+    {
+        this.oatFat = oatFat;
+        this.oatLiter = oatLiter;
+    }
+
+    public OatMilk(int oatFat, double oatLiter)
+    {
+        this.oatFat = (double) oatFat;
+        this.oatLiter = oatLiter;
+    }
+
+    public OatMilk(double oatFat, int oatLiter)
+    {
+        this.oatFat = oatFat;
+        this.oatLiter = (double)oatLiter;
+    }
     
+    public OatMilk(int oatFat, int oatLiter)
+    {
+        this.oatFat = (double) oatFat;
+        this.oatLiter = (double) oatLiter;
+    }
+    
+    public double getOatFat()
+    {
+        return oatFat;
+    }
+
+    public double getOatLiter()
+    {
+        return oatLiter;
+    }
+
 }
--- src/Pipe.java
+++ src/Pipe.java
@@ -1,4 +1,22 @@
 public class Pipe
 {
+    private int pipeDiam;
+    private int pipeLength;
+
+    public Pipe(int pipeDiam, int pipeLength)
+    {
+        this.pipeDiam = pipeDiam;
+        this.pipeLength = pipeLength;
+    }
+
+    public int getPipeDiam()
+    {
+        return pipeDiam;
+    }
+
+    public int getPipeLength()
+    {
+        return pipeLength;
+    }
     
 }
